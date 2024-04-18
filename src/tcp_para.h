66dbd80993a8d5ed39be4004ee7bbe17c017c677
OEmiliatanO <emiliaistruelove@gmail.com>
2024-04-19 03:20:25 +0800

    add window scaling option

--- src/tcp_para.h
+++ src/tcp_para.h
@@ -5,6 +5,8 @@ constexpr unsigned int RTT = 20; // ms
 constexpr size_t MSS = 1000; // byte
 constexpr size_t threshold = 64000; // byte
 constexpr size_t buffer_size = 512000; // byte
+constexpr size_t init_rwnd = 64000;
+constexpr size_t window_scale = 3; // init_rwnd << windown_scale = buffer_size
 constexpr int UNDEFINED = -1;
 
 #endif
